a1e44f17a260e930d2b6232cc83a6ceeddded541
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-03-13 16:04:16 +0000

    update benchmarks, record performance differences

--- bench.cpp
+++ bench.cpp
@@ -4,44 +4,54 @@
 
 #include <cstdint>
 
+constexpr int size = 4096;
+constexpr int half_size = size / 2;
+
 struct object_t
 {
-  int64_t data_[128];
+  char data_[size];
 };
 
 static void iterate_lookup(benchmark::State& state)
 {
   thh::lookup_table_t<std::string, object_t> look_up_table;
-  for (int i = 0; i < 128; ++i) {
+  look_up_table.reserve(state.range(0));
+  for (int i = 0; i < state.range(0); ++i) {
     look_up_table.insert({std::string("name") + std::to_string(i), object_t{}});
   }
 
   for ([[maybe_unused]] auto _ : state) {
-    std::for_each(look_up_table.begin(), look_up_table.end(), [](auto& value) {
-      value.data_[32] = 1234;
-    });
+    char data_element = 0;
+    std::for_each(
+      look_up_table.begin(), look_up_table.end(),
+      [&data_element](auto& value) { data_element = value.data_[half_size]; });
+    benchmark::DoNotOptimize(data_element);
     benchmark::ClobberMemory();
   }
 }
 
-BENCHMARK(iterate_lookup);
+BENCHMARK(iterate_lookup)->RangeMultiplier(2)->Range(32, 8 << 12);
 
 static void iterate_map(benchmark::State& state)
 {
   std::unordered_map<std::string, object_t> look_up_map;
-  for (int i = 0; i < 128; ++i) {
+  look_up_map.reserve(state.range(0));
+  for (int i = 0; i < state.range(0); ++i) {
     look_up_map.insert({std::string("name") + std::to_string(i), object_t{}});
   }
 
   for ([[maybe_unused]] auto _ : state) {
-    std::for_each(look_up_map.begin(), look_up_map.end(), [](auto& value) {
-      value.second.data_[32] = 1234;
-    });
+    char data_element = 0;
+    std::for_each(
+      look_up_map.begin(), look_up_map.end(), [&data_element](auto& value) {
+        data_element = value.second.data_[half_size];
+      });
+    benchmark::DoNotOptimize(data_element);
     benchmark::ClobberMemory();
   }
 }
 
-BENCHMARK(iterate_map);
+BENCHMARK(iterate_map)->RangeMultiplier(2)->Range(32, 8 << 12);
 
 static void find_lookup(benchmark::State& state)
 {
@@ -51,7 +61,8 @@ static void find_lookup(benchmark::State& state)
   }
 
   for ([[maybe_unused]] auto _ : state) {
-    look_up_table.call("name24", [](auto& value) { value.data_[32] = 1234; });
+    look_up_table.call(
+      "name24", [](auto& value) { value.data_[half_size] = (char)1; });
     benchmark::ClobberMemory();
   }
 }
@@ -67,7 +78,7 @@ static void find_map(benchmark::State& state)
 
   for ([[maybe_unused]] auto _ : state) {
     if (auto found = look_up_map.find("name24"); found != look_up_map.end()) {
-      found->second.data_[32] = 1234;
+      found->second.data_[half_size] = (char)1;
     }
     benchmark::ClobberMemory();
   }
