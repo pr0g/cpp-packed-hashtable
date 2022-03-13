72d17036b4cfcfd07b024fc6499f82316ec8d1c0
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-03-13 14:54:13 +0000

    add memory tracking examples

--- main.cpp
+++ main.cpp
@@ -1,25 +1,84 @@
 #include <thh-map/thh-map.hpp>
 
-#include <string>
+#include <array>
 #include <iostream>
+#include <string>
+
+// debug memory tracking code
+int g_total = 0;
+void* operator new(std::size_t n)
+{
+  g_total += n;
+  return malloc(n);
+}
+
+void operator delete(void* memory) noexcept
+{
+  free(memory);
+}
 
 int main(int argc, char** argv)
 {
   struct object_t
   {
-    int x;
-    int y;
+    char data_[4096];
   };
 
-  thh::lookup_table_t<std::string, object_t> lookup_table;
+  const std::array sizes{32,   64,   128,  256,    512,   1024,
+                         2048, 4096, 8192, 16'384, 32'768};
 
-  lookup_table.insert({"hello", object_t{3, 4}});
+  std::cout << "vector\n";
+  for (const int size : sizes) {
+    std::vector<object_t> vec;
+    vec.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      vec.push_back(object_t{});
+    }
+    std::cout << g_total << '\n';
+    g_total = 0;
+  }
 
-  lookup_table.call(
-    "hello",
-    [](object_t& object) {
-      std::cout << "object {" << object.x << ", " << object.y << "}\n";
-    });
+  std::cout << '\n';
 
-    return 0;
+  std::cout << "unordered_map\n";
+  for (const int size : sizes) {
+    std::unordered_map<std::string, object_t> map;
+    map.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      map.insert(std::pair(std::to_string(i), object_t{}));
+    }
+    std::cout << g_total << '\n';
+    g_total = 0;
+  }
+
+  std::cout << '\n';
+
+  // 16 bytes (per internal handles)
+  // 4 bytes (per element id)
+  // X bytes (per T)
+  std::cout << "handle\n";
+  for (const int size : sizes) {
+    thh::handle_vector_t<object_t> handle;
+    handle.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      auto h = handle.add();
+    }
+    std::cout << g_total << '\n';
+    g_total = 0;
+  }
+
+  std::cout << '\n';
+
+  std::cout << "lookup\n";
+  for (const int size : sizes) {
+    thh::lookup_table_t<std::string, object_t> lookup_table;
+    lookup_table.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      lookup_table.insert(std::pair(std::to_string(i), object_t{}));
+    }
+    std::cout << g_total << '\n';
+    g_total = 0;
+  }
+
+  return 0;
 }
