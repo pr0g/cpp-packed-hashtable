e3044621e5d5857c93ce5232a44cc26306750ec9
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-03-13 16:13:05 +0000

    update benchmarks for find

--- bench.cpp
+++ bench.cpp
@@ -56,34 +56,42 @@ BENCHMARK(iterate_map)->RangeMultiplier(2)->Range(32, 8 << 12);
 static void find_lookup(benchmark::State& state)
 {
   thh::lookup_table_t<std::string, object_t> look_up_table;
-  for (int i = 0; i < 128; ++i) {
+  look_up_table.reserve(state.range(0));
+  for (int i = 0; i < state.range(); ++i) {
     look_up_table.insert({std::string("name") + std::to_string(i), object_t{}});
   }
 
+  const auto lookup = std::string("name") + std::to_string(half_size);
   for ([[maybe_unused]] auto _ : state) {
-    look_up_table.call(
-      "name24", [](auto& value) { value.data_[half_size] = (char)1; });
+    char data_element = 0;
+    look_up_table.call(lookup, [&data_element](auto& value) {
+      data_element = value.data_[half_size];
+    });
     benchmark::ClobberMemory();
   }
 }
 
-BENCHMARK(find_lookup);
+BENCHMARK(find_lookup)->RangeMultiplier(2)->Range(32, 8 << 12);
 
 static void find_map(benchmark::State& state)
 {
   std::unordered_map<std::string, object_t> look_up_map;
-  for (int i = 0; i < 128; ++i) {
+  look_up_map.reserve(state.range());
+  for (int i = 0; i < state.range(); ++i) {
     look_up_map.insert({std::string("name") + std::to_string(i), object_t{}});
   }
 
+  const auto lookup = std::string("name") + std::to_string(half_size);
   for ([[maybe_unused]] auto _ : state) {
-    if (auto found = look_up_map.find("name24"); found != look_up_map.end()) {
-      found->second.data_[half_size] = (char)1;
+    char data_element = 0;
+    if (auto found = look_up_map.find(lookup); found != look_up_map.end()) {
+      data_element = found->second.data_[half_size];
     }
+    benchmark::DoNotOptimize(data_element);
     benchmark::ClobberMemory();
   }
 }
 
-BENCHMARK(find_map);
+BENCHMARK(find_map)->RangeMultiplier(2)->Range(32, 8 << 12);
 
 BENCHMARK_MAIN();
