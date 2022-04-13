08803b88c70ee37909e41bfb76dbc6303e28c831
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-04-13 20:39:35 +0100

    updates to memory tracking

--- memory.cpp
+++ memory.cpp
@@ -22,6 +22,8 @@ void operator delete(void* memory) noexcept
 template<int32_t Size>
 void run_memory_tracking()
 {
+  using std::string_literals::operator""s;
+
   struct object_t
   {
     char data_[Size];
@@ -30,80 +32,103 @@ void run_memory_tracking()
   const std::array sizes{32,   64,   128,  256,    512,    1024,
                          2048, 4096, 8192, 16'384, 32'768, 65'536};
 
-  auto underline_fn = [](size_t length) { return std::string(length, '-'); };
+  auto underline_fn = [](const size_t length) {
+    return std::string(length, '-');
+  };
 
-  // const std::string vector_name("std::vector");
-  // std::cout << vector_name << '\n' << underline_fn(vector_name.size()) << '\n';
-  // for (const int size : sizes) {
-  //   std::vector<object_t> vec;
-  //   vec.reserve(size);
-  //   for (int i = 0; i < size; ++i) {
-  //     vec.push_back(object_t{});
-  //   }
-  //   std::cout << std::left << std::setw(10) << g_total << std::right
-  //             << std::setw(2) << '(' << size << ")\n";
-  //   g_total = 0;
-  // }
+  const std::string vector_name =
+    "std::vector - elem size: "s + std::to_string(Size);
+  std::cout << vector_name << '\n' << underline_fn(vector_name.size()) << '\n';
+  for (const int size : sizes) {
+    std::vector<object_t> vec;
+    vec.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      vec.push_back(object_t{});
+    }
+    std::cout << std::left << std::setw(10) << g_total << std::right
+              << std::setw(2) << '(' << size << ")\n";
+    g_total = 0;
+  }
 
-  // std::cout << '\n';
+  std::cout << '\n';
 
-  // const std::string unordered_map_name("std::unordered_map");
-  // std::cout << unordered_map_name << '\n'
-  //           << underline_fn(unordered_map_name.size()) << '\n';
-  // for (const int size : sizes) {
-  //   std::unordered_map<std::string, object_t> map;
-  //   map.reserve(size);
-  //   for (int i = 0; i < size; ++i) {
-  //     map.insert(std::pair(std::to_string(i), object_t{}));
-  //   }
-  //   std::cout << std::left << std::setw(10) << g_total << std::right
-  //             << std::setw(2) << '(' << size << ")\n";
-  //   g_total = 0;
-  // }
+  const std::string unordered_map_name =
+    "std::unordered_map - elem size: "s + std::to_string(Size);
+  std::cout << unordered_map_name << '\n'
+            << underline_fn(unordered_map_name.size()) << '\n';
+  for (const int size : sizes) {
+    std::unordered_map<std::string, object_t> map;
+    map.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      map.insert(std::pair(std::to_string(i), object_t{}));
+    }
+    std::cout << std::left << std::setw(10) << g_total << std::right
+              << std::setw(2) << '(' << size << ")\n";
+    g_total = 0;
+  }
 
-  // std::cout << '\n';
+  std::cout << '\n';
 
-  // const std::string map_name("std::map");
-  // std::cout << map_name << '\n' << underline_fn(map_name.size()) << '\n';
-  // for (const int size : sizes) {
-  //   std::map<std::string, object_t> map;
-  //   for (int i = 0; i < size; ++i) {
-  //     map.insert(std::pair(std::to_string(i), object_t{}));
-  //   }
-  //   std::cout << std::left << std::setw(10) << g_total << std::right
-  //             << std::setw(2) << '(' << size << ")\n";
-  //   g_total = 0;
-  // }
+  const std::string map_name = "std::map - elem size: "s + std::to_string(Size);
+  std::cout << map_name << '\n' << underline_fn(map_name.size()) << '\n';
+  for (const int size : sizes) {
+    std::map<std::string, object_t> map;
+    for (int i = 0; i < size; ++i) {
+      map.insert(std::pair(std::to_string(i), object_t{}));
+    }
+    std::cout << std::left << std::setw(10) << g_total << std::right
+              << std::setw(2) << '(' << size << ")\n";
+    g_total = 0;
+  }
 
-  // std::cout << '\n';
+  std::cout << '\n';
 
-  // // 16 bytes (per internal handles)
-  // // 4 bytes (per element id)
-  // // X bytes (per T)
-  // const std::string handle_vector_name("thh::handle_vector_t");
-  // std::cout << handle_vector_name << '\n'
-  //           << underline_fn(handle_vector_name.size()) << '\n';
-  // for (const int size : sizes) {
-  //   thh::handle_vector_t<object_t> handle;
-  //   handle.reserve(size);
-  //   for (int i = 0; i < size; ++i) {
-  //     handle.add();
-  //   }
-  //   std::cout << std::left << std::setw(10) << g_total << std::right
-  //             << std::setw(2) << '(' << size << ")\n";
-  //   g_total = 0;
-  // }
+  // 16 bytes (per internal handles)
+  // 4 bytes (per element id)
+  // X bytes (per T)
+  const std::string handle_vector_name =
+    "thh::handle_vector_t - elem size: "s + std::to_string(Size);
+  std::cout << handle_vector_name << '\n'
+            << underline_fn(handle_vector_name.size()) << '\n';
+  for (const int size : sizes) {
+    thh::handle_vector_t<object_t> handle;
+    handle.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      handle.add();
+    }
+    std::cout << std::left << std::setw(10) << g_total << std::right
+              << std::setw(2) << '(' << size << ")\n";
+    g_total = 0;
+  }
 
-  // std::cout << '\n';
+  std::cout << '\n';
 
-  const std::string packed_hashtable_name("thh::packed_hashtable_t");
+  const std::string packed_hashtable_name =
+    "thh::packed_hashtable_t - elem size: " + std::to_string(Size);
   std::cout << packed_hashtable_name << '\n'
             << underline_fn(packed_hashtable_name.size()) << '\n';
   for (const int size : sizes) {
-    thh::packed_hashtable_t<std::string, object_t> lookup_table;
-    lookup_table.reserve(size);
+    thh::packed_hashtable_t<std::string, object_t> packed_hashtable;
+    packed_hashtable.reserve(size);
     for (int i = 0; i < size; ++i) {
-      lookup_table.add(std::pair(std::to_string(i), object_t{}));
+      packed_hashtable.add(std::pair(std::to_string(i), object_t{}));
+    }
+    std::cout << std::left << std::setw(10) << g_total << std::right
+              << std::setw(2) << '(' << size << ")\n";
+    g_total = 0;
+  }
+
+  std::cout << '\n';
+
+  const std::string packed_hashtable_rl_name =
+    "thh::packed_hashtable_rl_t - elem size: "s + std::to_string(Size);
+  std::cout << packed_hashtable_rl_name << '\n'
+            << underline_fn(packed_hashtable_rl_name.size()) << '\n';
+  for (const int size : sizes) {
+    thh::packed_hashtable_rl_t<std::string, object_t> packed_hashtable_rl;
+    packed_hashtable_rl.reserve(size);
+    for (int i = 0; i < size; ++i) {
+      packed_hashtable_rl.add(std::pair(std::to_string(i), object_t{}));
     }
     std::cout << std::left << std::setw(10) << g_total << std::right
               << std::setw(2) << '(' << size << ")\n";
